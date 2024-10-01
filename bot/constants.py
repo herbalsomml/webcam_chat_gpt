7cc375859e20404a6e3da286ca0847e47f7aa445
Kirill Suvorov <130235885+herbalsomml@users.noreply.github.com>
2024-10-01 20:42:38 +0200

    Create constants.py

---
+++ bot/constants.py
@@ -0,0 +1,9 @@
+import config
+
+HERBAL = ["хербал", "herbal", "кирил", "sommelier", "сомелье", "сомельер"]
+RATE_API_KEY = config.rate_api_key
+RATE_URL = f'https://api.currencybeacon.com/v1/latest?api_key={RATE_API_KEY}'
+
+STAT_URL = "https://cbexplorer.com/api/recent_broadcaster_stats.json"
+
+TOP_URL = "https://chaturbate.com/affiliates/api/onlinerooms/?format=json&wm=rvN8K"
